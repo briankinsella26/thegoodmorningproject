6bbe933433e9ecdd478fcdbc5ee9cf8143de5783
brian kinsella <16728365@mail.wit.ie>
2021-12-22 19:47:08 +0000

    logic fix python sub script

--- pi_scripts/user_details.py
+++ pi_scripts/user_details.py
@@ -4,8 +4,10 @@ import datetime
 import requests
 import json
 
+playbackStampRecords = [{"b500c6b0-633b-11ec-85c5-cba80427674d2021-10-10"}]
+
 def getConfigurations():
-    url = "https://data.mongodb-api.com/app/data-mtybs/endpoint/data/beta/action/findOne"
+    url = "https://data.mongodb-api.com/app/data-mtybs/endpoint/data/beta/action/find"
     payload = json.dumps({
         "collection": "configurations",
         "database": "themorningprojectdb",
@@ -17,45 +19,37 @@ def getConfigurations():
         'api-key': 'kvErM5pzFQaISsF733UpenYeDTT7bWrJ85mAxhz956wb91U5igFxsJoDEDpyW6NJ'
     }
     response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+    jsonResponse = response.json()
 
+    return jsonResponse['documents']
 
 
 def isNotificationScheduled():
-    getConfigurations()
-    with open("/home/pi/git/thegoodmorningproject/models/configuration-store.json") as jsonFile:
-        jsonObject = json.load(jsonFile)
-        jsonFile.close()
-        
+    configList = getConfigurations()
     currentDate = datetime.datetime.now().date()
     currentTime = datetime.datetime.now().strftime("%H:%M")
-    configList = jsonObject['configurationCollection']
     for config in configList:
-        if config["settings"][0]["playbackDateStamp"] != str(currentDate) and currentTime >= config["settings"][0]["starttime"] and currentTime <= config["settings"][0]["endtime"]:
+        if config["id"]+str(currentDate) not in playbackStampRecords and currentTime >= config['settings'][0]['starttime'] and currentTime <= config['settings'][0]['endtime']:
             return True
 
-def getUserCommands():
-        with open("/home/pi/git/thegoodmorningproject/models/configuration-store.json") as jsonFile:
-            jsonObject = json.load(jsonFile)
-            jsonFile.close()
-            
-            currentDate = datetime.datetime.now().date()
-            currentTime = datetime.datetime.now().strftime("%H:%M")
-            configList = jsonObject['configurationCollection']
-            for config in configList:
-                if config["settings"][0]["playbackDateStamp"] != currentDate and currentTime >= config["settings"][0]["starttime"] and currentTime <= config["settings"][0]["endtime"]:
-                    name = config["settings"][0]["name"]
-                    playbackInformation = config["settings"][0]["info"]
-                    #add date stamp to config to avoid repeating notifications
-                    config["settings"][0]["playbackDateStamp"] = str(currentDate)
-                    #set time of day descriptor    
-                    if currentTime >= '00:00'and currentTime < '12:00':
-                        dayTimeDescriptor = 'morning'
-                    elif currentTime >= '12:00' and currentTime <= '16:00':
-                        dayTimeDescriptor = 'afternoon'
-                    else: 
-                        dayTimeDescriptor = 'night'
-                    with open("/home/pi/git/thegoodmorningproject/models/configuration-store.json", "w") as jsonFile:
-                        json.dump(jsonObject, jsonFile)
-                    return [dayTimeDescriptor, name, playbackInformation]
+def getUserConfiguration():
+    configList = getConfigurations()
+    currentDate = datetime.datetime.now().date()
+    currentTime = datetime.datetime.now().strftime("%H:%M")
+
+    for config in configList:
+        if config["id"]+str(currentDate) not in playbackStampRecords and currentTime >= config['settings'][0]['starttime'] and currentTime <= config['settings'][0]['endtime']:
+            email = config['settings'][0]['amemail']
+            password = config['settings'][0]['ampassword'] 
+            name = config['settings'][0]['name']
+            playbackInformation = config['settings'][0]['info'] 
+            if currentTime >= '00:00'and currentTime < '12:00':
+                dayTimeDescriptor = 'morning'
+            elif currentTime >= '12:00' and currentTime <= '16:00':
+                dayTimeDescriptor = 'afternoon'
+            else: 
+                dayTimeDescriptor = 'night'   
+            playbackStampRecords.append(config['id']+str(currentDate))
+            return [email, password, dayTimeDescriptor, name, playbackInformation]
+
 
